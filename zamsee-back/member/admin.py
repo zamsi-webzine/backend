f8b0556575c7176fa3273527dc90c3bdbbe18f48
huskyhoochu <dfg1499@gmail.com>
2018-03-09 00:09:12 +0900

    Create Admin page

--- zamsee-back/member/admin.py
+++ zamsee-back/member/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import ZSUser
+from .forms import UserChangeForm, UserCreationForm
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'nickname', 'is_active', 'is_admin', 'date_joined',)
+    list_filter = ('date_joined',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        (_('Personal info'), {'fields': ('nickname',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2',),
+        }
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('-pk',)
+    filter_horizontal = ()
+
+
+admin.site.register(ZSUser, UserAdmin)
+admin.site.unregister(Group)
