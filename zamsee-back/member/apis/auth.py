d37aeddff54402bea966b52bbd3a78a4871ad03b
huskyhoochu <dfg1499@gmail.com>
2018-03-10 16:18:48 +0900

    Create login view

--- zamsee-back/member/apis/auth.py
+++ zamsee-back/member/apis/auth.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -14,7 +16,12 @@ class Login(APIView):
             password=password
         )
         if user:
-            context = {
-                'nickname': user.nickname
+            token, token_created = Token.objects.get_or_create(user=user)
+            data = {
+                'token': token.key
             }
-            return Response(context)
+            return Response(data, status=status.HTTP_200_OK)
+        data = {
+            'message': 'Invalid Credentials'
+        }
+        return Response(data, status=status.HTTP_401_UNAUTHORIZED)
