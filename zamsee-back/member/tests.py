fb547c6bc52d79e55907da0ab8659b6e751fa87e
huskyhoochu <dfg1499@gmail.com>
2018-03-21 10:51:47 +0900

    Create first test: signup-url-name

--- zamsee-back/member/tests.py
+++ zamsee-back/member/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APILiveServerTestCase
 
-# Create your tests here.
+from .apis import Signup
+
+
+class UserSignUpTest(APILiveServerTestCase):
+    def setUp(self):
+        self.URL_API_SIGNUP_NAME = 'authentication:signup'
+        self.URL_API_SIGNUP = '/auth/signup/'
+        self.SIGNUP_VIEW_CLASS = Signup
+
+    def test_signup_url_name_reverse(self):
+        url = reverse(self.URL_API_SIGNUP_NAME)
+        self.assertEqual(url, self.URL_API_SIGNUP)
