588932c31e4fbb8d0a5bff1812aa3dcac49ae9d1
huskyhoochu <dfg1499@gmail.com>
2018-05-23 17:48:33 +0900

    Add author field in Post model

--- zamsee-back/post/models.py
+++ zamsee-back/post/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.SlugField(max_length=100)
+    post = models.TextField(blank=True, null=True)
