e918e7dae78d2cc79af09089247df26f9c8b76fd
huskyhoochu <dfg1499@gmail.com>
2018-05-23 17:48:48 +0900

    Create PostCreateList view

---
+++ zamsee-back/post/apis.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
+
+from post.models import Post
+from .serializers import PostSerializer
+
+User = get_user_model()
+
+
+class PostCreateList(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    parser_classes = (MultiPartParser, FormParser,)
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        user = User.objects.get(pk=self.kwargs['author_pk'])
+        return serializer.save(author=user)
---
+++ zamsee-back/post/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from member.serializers import UserSerializer
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'pk',
+            'author',
+            'title',
+            'post',
+        )
