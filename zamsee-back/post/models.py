0b7037574bfaa3f344157f7d8087f2926cb6897f
huskyhoochu <dfg1499@gmail.com>
2018-05-25 16:40:48 +0900

    Add fields in post model & Make migration

--- zamsee-back/post/models.py
+++ zamsee-back/post/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.SlugField(max_length=100)
     post = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
