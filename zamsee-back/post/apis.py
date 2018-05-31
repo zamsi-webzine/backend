2b9ecbf539a10a5cf419e02023deefb0cf9c69cf
huskyhoochu <dfg1499@gmail.com>
2018-05-31 23:26:46 +0900

    Create PostRetrieve method

--- zamsee-back/post/apis.py
+++ zamsee-back/post/apis.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated
 
@@ -20,3 +20,13 @@ class PostCreateList(ListCreateAPIView):
     def perform_create(self, serializer):
         user = User.objects.get(pk=self.kwargs['author_pk'])
         return serializer.save(author=user)
+
+
+class PostRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = PostSerializer
+    lookup_url_kwarg = 'post_pk'
+
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        return queryset.filter(author_id=self.kwargs['author_pk'])
--- zamsee-back/post/urls.py
+++ zamsee-back/post/urls.py
@@ -6,4 +6,5 @@ app_name = 'post'
 
 urlpatterns = [
     path('<int:author_pk>/', apis.PostCreateList.as_view(), name='create_list'),
+    path('<int:author_pk>/<int:post_pk>/', apis.PostRetrieveUpdateDestroy.as_view(), name='detail'),
 ]
