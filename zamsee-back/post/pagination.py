dd9d16155010f4e0615cc10ec37e82b3dcddf87c
huskyhoochu <dfg1499@gmail.com>
2018-06-02 15:59:06 +0900

    Debug pagination option

--- zamsee-back/post/pagination.py
+++ zamsee-back/post/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class PostPagination(PageNumberPagination):
     page_size = 6
-    page_size_query_param = 'page'
-    max_page_size = 10000
+    page_query_param = 'page'
+    max_page_size = 100
